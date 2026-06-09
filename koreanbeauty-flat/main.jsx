32afe93881db2aa8580ea199fba2a9a3de7b27b3
KoreanBeautySwiss <pedrobaiao08@gmail.com>
2026-06-09 21:50:57 +0200

    Add files via upload

---
+++ koreanbeauty-flat/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import KoreanBeauty from "./KoreanBeauty.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <KoreanBeauty />
+  </React.StrictMode>
+);
